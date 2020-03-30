a2b66741bbc37222c2de50e44a089445f187b8d3
Rebecca Berrens <Rebecca.Berrens@cruk.cam.ac.uk>
2020-03-30 13:43:28 +0100

    added genome preparation

--- Methylation_seq/bash_files/mapping.sh
+++ Methylation_seq/bash_files/mapping.sh
@@ -4,9 +4,12 @@ fastqc ${NAME}
 # trimming sequences using trim-galore v0.6.5
 trim_galore --paired --length 25 --trim-n --clip_R2 5 --fastqc ${READ1} ${READ2}
 
+# bismark (v0.22.3) genome preparation
+bismark_genome_preparation --path_to_aligner ${BOWTIE2} --verbose ${ENSEMBL_genome/GRCm38}
+
 # bismark alignment v0.22.3 for alignment, algined to genome GRCm38 with bowtie2-2.3.5
 bismark --score_min L,0,-0.4 \
---genome /hps/research1/marioni/Rebecca/annotation/mouse/ENSEMBL_genome/ \
+--genome annotation/mouse/ENSEMBL_genome/ \
 -1 ${READ1} \
 -2 ${READ2}
 
